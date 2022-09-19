969788838589098f60a604dddf4b443d6441419f
rogergelio <87195114+rogergelio@users.noreply.github.com>
2022-09-19 10:21:20 -0500

    lang

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -56,7 +56,7 @@ const ProductDetails = ({ product, products }) => {
       </div>
 
       <div className="maylike-products-wrapper">
-          <h2>You may also like</h2>
+          <h2>Puede que te guste: </h2>
           <div className="marquee">
             <div className="maylike-products-container track">
               {products.map((item) => (
